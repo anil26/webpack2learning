4f7adf80a0e6e6cdfd91dc1f0878768cc3638448
Chaudhary <ACHAUDHARY@beckman.com>
2017-06-02 14:15:50 +0530

    first commit webpack learning

---
+++ src/image_viewer.js
@@ -0,0 +1,14 @@
+import big1 from '../assets/testbig1.jpg';
+import big2 from '../assets/testbig2.jpg';
+import big3 from '../assets/testbig3.jpg';
+import small1  from '../assets/testsmall1.jpg';
+import small2 from '../assets/testsmall2.jpg';
+import small3 from '../assets/testsmall3.jpg';
+import '../styles/image_viewer.css';
+const image = document.createElement('img');
+image.src=small1;
+document.body.appendChild(image);
+
+const bigimage = document.createElement('img');
+bigimage.src=big1;
+document.body.appendChild(bigimage);
---
+++ src/index.js
@@ -0,0 +1,5 @@
+//const sum = require('./sum');
+import sum from './sum';
+import './image_viewer';
+const total = sum(2,3);
+console.log(total);
